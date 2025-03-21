89fc76d8d3712217bbba324d95df0a362057ab68
Avi KC <avikc.copy@gmail.com>
2025-03-21 19:10:55 +0545

    duck/duck-doc just got 1000x faster

---
+++ file.py
@@ -0,0 +1,54 @@
+
+'''
+What is this? A satyr on the life of web developers- those stupid people!
+If you are in love with software that doesn't complicate go to [Godsend](http://github.com/thisismars-x/godsend)
+I will someday start a vlog and write books on bettering engineers, because today engineers are obsolete, redundant except a few.
+'''
+
+def print_gives(a: str):
+	'''Prints what you give to it'''
+	print(a)
+
+def give_print(a, b):
+	'''Returns whatever you give to it'''
+	return (a, b)
+
+class Morocco:
+	'''Morocco is an awesome country.
+	It gets pretty warm in there, so do not bother for a sweater'''
+
+	def __init__(self):
+		pass
+
+	def express(self, a) -> bool:
+		'''Checks your input to match it against its database'''
+		return a == self.db
+
+	def dont_express(self, a):
+		'''Reverses whatever express does'''
+		return not a==self.db
+
+class Damnation:
+	'''What is damnation you ask? DAMNN'''
+	
+	def compile(self, status, **options) -> re.Pattern:
+		'''
+		Tries to compile to a compiled regex pattern.
+		**Usage**
+		`code 
+		pattern = compile(True, a=b, c=d)
+		if pattern:
+			if pattern is valid:
+				do something
+			else:
+				raise something
+		`
+		'''
+		pass
+
+	def kanye(self):
+		'''Print Kanye status'''
+		print("I AM THE GOATED")
+
+
+
